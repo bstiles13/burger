0d28a25cd2d6251a7d52b30292c7a014d2295201
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-05-19 05:37:55 -0700

    created file/folder layout

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table events.
+CREATE TABLE burgers (
+id INT AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR (100) NOT NULL,
+devoured BOOLEAN DEFAULT FALSE,
+date TIMESTAMP NOT NULL,
+PRIMARY KEY (id)
+);
