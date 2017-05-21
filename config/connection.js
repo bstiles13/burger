87498e458de2f4b02de048f274f4e0588c30980f
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-05-20 20:54:58 -0700

    testing heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,15 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'burgers_db'
-});
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "1234",
+        database: "burger_db"
+    });
 }
 
 connection.connect(function(err) {
