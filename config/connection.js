1e1b56efb6e07bf0bd4b323bf9ad7e122e5d2ce0
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-05-20 18:07:10 -0700

    added heroku/jaws

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 var mysql = require('mysql');
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
     database: 'burgers_db'
-})
+});
+}
 
 connection.connect(function(err) {
     console.log('Database connected.');
